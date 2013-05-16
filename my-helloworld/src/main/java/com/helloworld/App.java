50bbe7713a38d5f52b71c27de0fe2119ba05f970
Nuno Coelho <nco@qspin-fixe1.qspin.be>
2013-05-16 16:20:51 +0200

    Injected compilation error: check if build failed!!

--- my-helloworld/src/main/java/com/helloworld/App.java
+++ my-helloworld/src/main/java/com/helloworld/App.java
@@ -4,10 +4,12 @@ package com.helloworld;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Compilation Error Injection on purpose!!!!!
     }
 }
