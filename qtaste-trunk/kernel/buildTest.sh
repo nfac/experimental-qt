342cf8e5dbeba449330fe43e1568f905efd3b16c
Nuno Coelho <nco@qspin-fixe1.qspin.be>
2013-05-31 16:44:36 +0200

    update

--- qtaste-trunk/kernel/buildTest.sh
+++ qtaste-trunk/kernel/buildTest.sh
@@ -2,7 +2,7 @@
 
 # build using maven
 echo "NCO: Maven instal Kernel"
-mvn clean install assembly:single
+mvn clean install assembly:single -e -X
 
 # restore pom.xml if modified
 mv -f pom.xml.bak pom.xml >& /dev/null
