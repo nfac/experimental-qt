455a9f62b6616a02d53fa82a3afeb43d90ac209c
Nuno Coelho <nco@qspin-fixe1.qspin.be>
2013-06-03 16:32:57 +0200

    update

--- qtaste-trunk/kernel/src/test/java/com/qspin/qtaste/kernel/testapi/TestAPIImplTest.java
+++ qtaste-trunk/kernel/src/test/java/com/qspin/qtaste/kernel/testapi/TestAPIImplTest.java
@@ -34,20 +34,20 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public class TestAPIImplTest extends TestCase {
     private TestAPI instance;
-    
+
     public TestAPIImplTest(String testName) {
         super(testName);
         // Log4j Configuration
-		String path = StaticConfiguration.CONFIG_DIRECTORY + "/log4j.properties";
+        String path = StaticConfiguration.CONFIG_DIRECTORY + "/log4j.properties";
         PropertyConfigurator.configure(path.replaceAll(" ", "\\ "));
     }
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        
-    	// register component into the TestAPI
-		String path = StaticConfiguration.TESTBED_CONFIG_DIRECTORY + "/enginetest." + StaticConfiguration.TESTBED_CONFIG_FILE_EXTENSION;
+
+        // register component into the TestAPI
+        String path = StaticConfiguration.TESTBED_CONFIG_DIRECTORY + "/enginetest." + StaticConfiguration.TESTBED_CONFIG_FILE_EXTENSION;
         TestBedConfiguration.setConfigFile(path.replaceAll(" ", "\\ "));
         ComponentsLoader.getInstance();
         instance = TestAPIImpl.getInstance();
@@ -60,29 +60,22 @@ public class TestAPIImplTest extends TestCase {
     }
 
     /**
-     * Test of register method, of class TestAPIImpl.
-     */
-    public void testRegister() {
-        System.out.println("register");
-        String packageName = "JUNIT";
-        String component = "JUNIT";
-        ComponentFactory factory = null;
-        String method = "test";  
-
-        instance.register(packageName, component, factory, method);
-    }
-
-    /**
      * Test of getRegisteredComponents method, of class TestAPIImpl.
      */
     public void testGetRegisteredComponents() {
+        String packageName = "JUNIT";
+        String component = "JUNIT";
+        ComponentFactory factory = null;
+        String method = "test";
+        instance.register(packageName, component, factory, method);
+
         System.out.println("getRegisteredComponents");
-        
+
         Collection<String> result = instance.getRegisteredComponents();
-        
+
         assertNotNull(result);
-        
-        assertTrue("JUNIT should be a component as it has been previously added", result.contains("JUNIT"));                
+
+        assertTrue("JUNIT should be a component as it has been previously added", result.contains("JUNIT"));
     }
 
     /**
@@ -90,19 +83,19 @@ public class TestAPIImplTest extends TestCase {
      */
     public void testGetRegisteredVerbs() {
         System.out.println("getRegisteredVerbs");
-        String component = "nonexistingcomponent";        
-        
+        String component = "nonexistingcomponent";
+
         try {
-            instance.getRegisteredVerbs(component);        
+            instance.getRegisteredVerbs(component);
             fail("should throw a NoSuchElementException as component is not existing");
         }
         catch (NoSuchElementException e) {
             // should be catched
         }
 
-        component = "EngineTest";        
-        Collection<String> result = instance.getRegisteredVerbs(component);        
-        assertTrue("A existing component should contains should contain methods", result.size() > 0);        
+        component = "EngineTest";
+        Collection<String> result = instance.getRegisteredVerbs(component);
+        assertTrue("A existing component should contains should contain methods", result.size() > 0);
     }
 
 
@@ -113,7 +106,7 @@ public class TestAPIImplTest extends TestCase {
         System.out.println("getComponent");
         String component = "non-existingcomponent";
         TestData data = null;
-        
+
         try {
             instance.getComponent(component, data);
             fail("getComponent should throw a NoSuchElementException as component doesn't exist!");
@@ -124,12 +117,12 @@ public class TestAPIImplTest extends TestCase {
         catch (QTasteException e) {
             fail("getComponent should throw a NoSuchElementException as component doesn't exist!");
         }
-        
+
         // Please notice that EngineTest component use a SingletonFactory so data is not used!
         component = "EngineTest";
         try {
             Component result = instance.getComponent(component, data);
-            assertNotNull("The instance of EngineTest should not be null", result);                        
+            assertNotNull("The instance of EngineTest should not be null", result);
         }
         catch (QTasteException e) {
             fail("getComponent should not throw an QTasteException as fail as component can be instantiated!");
@@ -141,7 +134,7 @@ public class TestAPIImplTest extends TestCase {
      */
     public void testGetComponentName() {
         System.out.println("getComponentName");
-        
+
         // Please notice that EngineTest component use a SingletonFactory so data is not used!
         String component = "EngineTest";
         TestData data = null;
