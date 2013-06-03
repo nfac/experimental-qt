61a653ed297fcb0ae79dc92bc76dd1df9c10d1c8
Nuno Coelho <nco@qspin-fixe1.qspin.be>
2013-06-03 17:07:50 +0200

    update

--- qtaste-trunk/buildTest.sh
+++ qtaste-trunk/buildTest.sh
@@ -22,23 +22,3 @@ popd
 
 # build other
 mvn install || exit 1
-
-# build plugins
-pushd plugins_src
-./build.sh || exit 1
-popd
-
-# build demonstrations
-pushd demo
-./build.sh || exit 1
-popd
-
-# create installer
-pushd izpack
-./createInstaller.sh || exit 1
-popd
-
-# generate documentation
-pushd doc
-./generateDocs.sh || exit 1
-popd
