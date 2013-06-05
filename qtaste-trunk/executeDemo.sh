db8fea3ace56f510be4ac5934b882135f3b10dd5
nco-qspin <nco@qspin-fixe2.qspin.be>
2013-06-05 17:36:48 +0200

    added execution of testPlayback demo - 2 try

--- qtaste-trunk/executeDemo.sh
+++ qtaste-trunk/executeDemo.sh
@@ -2,10 +2,10 @@
 
 # execute Process
 pushd demo
+echo "Running Playback Test Suite"
+../bin/qtaste_start.sh -testsuite ./TestSuites/PlayBack/ -testbed ./Testbeds/playback.xml || exit 1
 echo "Running Process Test Suite"
 ../bin/qtaste_start.sh -testsuite ./TestSuites/Process/ -testbed ./Testbeds/process.xml || exit 1
 echo "Running Translate Test Suite"
 ../bin/qtaste_start.sh -testsuite ./TestSuites/TestTranslate/ -testbed ./Testbeds/demo_web.xml  || exit 1
-echo "Running Playback Test Suite"
-../bin/qtaste_start.sh -testsuite ./TestSuites/PlayBack/ -testbed ./Testbeds/playback.xml || exit 1
 popd
