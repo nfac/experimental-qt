862e8a93273762034717dbd78b724b3931cf9ac5
nco-qspin <nco@qspin-fixe2.qspin.be>
2013-06-05 16:52:32 +0200

    added execution of testTranslate demo

--- qtaste-trunk/executeDemo.sh
+++ qtaste-trunk/executeDemo.sh
@@ -6,4 +6,6 @@ echo "Running Process Test Suite"
 ../bin/qtaste_start.sh -testsuite ./TestSuites/Process/ -testbed ./Testbeds/process.xml || exit 1
 echo "Running Translate Test Suite"
 ../bin/qtaste_start.sh -testsuite ./TestSuites/TestTranslate/ -testbed ./Testbeds/demo_web.xml  || exit 1
+echo "Running Playback Test Suite"
+../bin/qtaste_start.sh -testsuite ./TestSuites/PlayBack/ -testbed ./Testbeds/playback.xml
 popd
