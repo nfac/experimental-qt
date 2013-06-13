37095a0c23325e0e818d88913445ab2910369f87
nco-qspin <nco@qspin-fixe2.qspin.be>
2013-06-13 17:41:59 +0200

    testing install xterm

--- build-test-qtaste.sh
+++ build-test-qtaste.sh
@@ -9,7 +9,7 @@ export DISPLAY=:99.0
 find . -name "*.sh" | xargs chmod +x
 
 #sudo apt-get install xterm
-yum install xterm
+gem install xterm
 
 Xvfb :99.0 &
 
--- qtaste-trunk/executeDemo.sh
+++ qtaste-trunk/executeDemo.sh
@@ -6,5 +6,9 @@ echo "Running All Linux Demo Campaign"
 #../bin/qtaste_campaign_start.sh ./TestCampaigns/AllLinuxDemo.xml || exit 1
 
 # ../bin/qtaste_start.sh -testsuite ./TestSuites/TestTranslate/ -testbed ./Testbeds/demo_web.xml OK
-../bin/qtaste_start.sh -testsuite ./TestSuites/PlayBack/ -testbed ./Testbeds/playback.xml
+# ../bin/qtaste_start.sh -testsuite ./TestSuites/PlayBack/ -testbed ./Testbeds/playback.xml OK
+
+# ../bin/qtaste_start.sh -testsuite ./TestSuites/Process/ -testbed ./Testbeds/process.xml NOK - xterm need to be installed
+
+
 popd
