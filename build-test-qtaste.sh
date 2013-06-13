641d4c9ece8f2c6a6cda420b500d24c82db88c4c
nco-qspin <nco@qspin-fixe2.qspin.be>
2013-06-13 15:13:16 +0200

    update

--- build-test-qtaste.sh
+++ build-test-qtaste.sh
@@ -5,15 +5,17 @@ echo "Executing script build-test-qtaste"
 export QTASTE_TRAVIS_CI="1"
 export DISPLAY=:99.0
 
-# before_install:
+# Before build configurations:
 find . -name "*.sh" | xargs chmod +x
 #sudo apt-get install xterm
 Xvfb :99.0 &
-# run build script:
-#pushd qtaste-trunk
-#./buildAll.sh || exit 1
-#popd
-# execute qtaste demo aftersuccess:
+
+# Build:
+pushd qtaste-trunk
+./buildAll.sh || exit 1
+popd
+
+# Execute qtaste demo after build success:
 pushd qtaste-trunk
 ./executeDemo.sh || exit 1
 popd
