f39ded93bbc69ffa38f1a3572d69c0dc12d9a210
nco-qspin <nco@qspin-fixe2.qspin.be>
2013-06-14 15:38:28 +0200

    test running a term

--- qtaste-trunk/executeDemo.sh
+++ qtaste-trunk/executeDemo.sh
@@ -5,7 +5,7 @@ pushd demo
 echo "Running All Linux Demo Campaign"
 #../bin/qtaste_campaign_start.sh ./TestCampaigns/AllLinuxDemo.xml || exit 1
 
-../bin/qtaste_start.sh -testsuite ./TestSuites/TestTranslate/ -testbed ./Testbeds/demo_web.xml || exit 1
-../bin/qtaste_start.sh -testsuite ./TestSuites/PlayBack/ -testbed ./Testbeds/playback.xml || exit 1
+../bin/qtaste_start.sh -testsuite ./TestSuites/TestTranslate/ -testbed ./Testbeds/demo_web.xml
+../bin/qtaste_start.sh -testsuite ./TestSuites/PlayBack/ -testbed ./Testbeds/playback.xml
 # ../bin/qtaste_start.sh -testsuite ./TestSuites/Process/ -testbed ./Testbeds/process.xml NOK - xterm need to be installed
 popd
