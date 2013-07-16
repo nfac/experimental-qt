b02d8d665bd7fff56c05896aa0cd3d572358a49f
nfac <nco@qspin-fixe2.qspin.be>
2013-07-16 14:08:09 +0200

    merged scripts: experimental to standart one

--- qtaste-trunk/kernel/build.sh
+++ qtaste-trunk/kernel/build.sh
@@ -5,12 +5,7 @@ pushd ../tools/jython/lib/Lib/
 rm -f *.class
 popd
 
-# build using maven
-if [ "$QTASTE_HOSTED_CI" = "1" ]; then
-  mvn clean install assembly:single -Denvironment=hosted_ci || exit 1
-else
-  mvn clean install assembly:single || exit 1
-fi
+mvn clean install -P qtaste-build-kernel-first -Denvironment=hosted_ci || exit 1
 
 # restore pom.xml if modified
 mv -f pom.xml.bak pom.xml >& /dev/null
